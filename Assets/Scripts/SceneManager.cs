33a96dd9bea83bb1387684a786428a71094e49b6
Khenan <52697992+Khenan@users.noreply.github.com>
2022-03-09 22:22:31 +0100

    update

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManager : MonoBehaviour
+{
+
+    [SerializeField, Tooltip("Slider de loading")]
+    private GameObject m_canvasLoader;
+    
+    void ChangeScene(string p_sceneName)
+    {
+        
+    }
+}
