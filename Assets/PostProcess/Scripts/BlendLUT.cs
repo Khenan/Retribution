c5ee62c92f9c50bc8945e8d6c88bc9074f5ebc57
Khenan <52697992+Khenan@users.noreply.github.com>
2022-03-19 11:21:24 +0100

    EditorSetting BlendLUT

--- Assets/PostProcess/Scripts/BlendLUT.cs
+++ Assets/PostProcess/Scripts/BlendLUT.cs
@@ -1,13 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 public class BlendLUT : MonoBehaviour
 {
     private GlobalVolumeBlendLUT m_GlobalVolume;
     [SerializeField, Tooltip("LUT Table")]
     private Texture2D m_myLutTable;
-
+    
+#if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        Handles.color = Color.magenta;
+        Handles.DrawWireCube(transform.position, transform.localScale);
+        Handles.color = Color.white;
+    }
+#endif
     private void Awake()
     {
         m_GlobalVolume = FindObjectOfType<GlobalVolumeBlendLUT>();
