65a5dc1d6e5b40b92240eb4d1bb9c559263268bd
Khenan <52697992+Khenan@users.noreply.github.com>
2022-04-06 16:24:23 +0200

    add walk RDC sound

--- Assets/Scripts/Player/PlayerSound.cs
+++ Assets/Scripts/Player/PlayerSound.cs
@@ -14,15 +14,15 @@ public class PlayerSound : MonoBehaviour
             Debug.Log("is moving");
             if (m_walkStart) return;
             Debug.Log("Lance le son");
-            SoundManager.Instance.Play(SoundManager.Instance.m_playerWalkTop);
-            SoundManager.Instance.m_playerWalkTop.m_event.SetParameter("Progression", 0);
+            SoundManager.Instance.Play(SoundManager.Instance.m_playerWalkBottom);
+            SoundManager.Instance.m_playerWalkBottom.m_event.SetParameter("Progression", 0);
             m_walkStart = true;
             return;
         }
 
         if (!m_walkStart) return;
         Debug.Log("Arrets le son");
-        SoundManager.Instance.m_playerWalkTop.m_event.SetParameter("Progression", 1);
+        SoundManager.Instance.m_playerWalkBottom.m_event.SetParameter("Progression", 1);
         m_walkStart = false;
     }
 }
