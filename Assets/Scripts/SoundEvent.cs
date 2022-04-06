79b80c394bded47438386605a2a09cff296938fe
Khenan <52697992+Khenan@users.noreply.github.com>
2022-04-06 15:09:28 +0200

    add sound branch

---
+++ Assets/Scripts/SoundEvent.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+
+[RequireComponent(typeof(StudioEventEmitter))]
+public class SoundEvent : MonoBehaviour
+{
+    public StudioEventEmitter m_event;
+
+    private void Awake()
+    {
+        m_event = GetComponent<StudioEventEmitter>();
+    }
+
+    public void Play()
+    {
+        Debug.Log("PLAY !");
+        m_event.Play();
+    }
+    public void Stop()
+    {
+        Debug.Log("STOP !");
+        m_event.Stop();
+    }
+}
