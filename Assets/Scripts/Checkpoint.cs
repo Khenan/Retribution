5acd050523fa6df997015f3296884d2b4ec6965b
Khenan <52697992+Khenan@users.noreply.github.com>
2022-05-18 16:00:44 +0200

    update

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
@@ -9,6 +10,28 @@ public class Checkpoint : MonoBehaviour
 
     public bool m_repop = false;
     public bool m_finish = false;
+
+    [SerializeField, Tooltip("ActiveOnStart")]
+    private bool m_activeOnStart = true;
+    
+    [SerializeField, Tooltip("Event à écouter pour activé le box collider")] private Event m_eventToActive;
+
+    private void OnEnable()
+    {
+        if (m_activeOnStart) return;
+        GetComponent<BoxCollider>().enabled = false;
+        m_eventToActive.m_event += Active;
+    }
+    private void OnDisable()
+    {
+        if (m_activeOnStart) return;
+        m_eventToActive.m_event -= Active;
+    }
+
+    private void Active()
+    {
+        GetComponent<BoxCollider>().enabled = true;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
