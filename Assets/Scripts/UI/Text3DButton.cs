b2c604189367850b7c30ca0bc90bc1cadf14cd79
Khenan <52697992+Khenan@users.noreply.github.com>
2022-05-25 16:13:40 +0200

    idpaet

--- Assets/Scripts/UI/Text3DButton.cs
+++ Assets/Scripts/UI/Text3DButton.cs
@@ -12,6 +12,8 @@ public class Text3DButton : MonoBehaviour
     [SerializeField, Tooltip("Couleur au moment du Hover du pointeur")] private Color m_hoverColor;
     [SerializeField, Tooltip("Index de la scène à donner"), Range(0, 3)] private int m_sceneIndex = 0;
     [SerializeField, Tooltip("Le Bloqueur de cursor pour éviter de cliquer sur un bouton après avoir cliqué sur un")] private BoxCollider m_bloqueur;
+
+    [SerializeField] private bool m_quitButton;
     private void Awake()
     {
         m_tmpro = GetComponent<TextMeshPro>();
@@ -27,6 +29,11 @@ public class Text3DButton : MonoBehaviour
     }
     private void OnMouseUp()
     {
+        if (m_quitButton)
+        {
+            Application.Quit();
+            return;
+        }
         m_bloqueur.enabled = true;
         SceneManager.Instance.ChangeScene(m_sceneIndex);
     }
